7f4c07d5b88fa34e7a247f83ff855fb8931ba41e
Jasdeep Madan <jasdeepm@gmail.com>
2016-08-29 16:58:35 +0530

    removed unsed plugins, added loaders for css, jpg, svg, fonts etc

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,39 @@ let webpack = require('webpack');
 let path = require('path');
 let HtmlWebpackPlugin = require('html-webpack-plugin');
 const validate = require('webpack-validator');
-const MergePlugin = require('webpack-merge');
-let parts = require('./lib/webpack.parts');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+// const MergePlugin = require('webpack-merge');
+// let parts = require('./lib/webpack.parts');
 
 const PATHS = {
     app: path.join(__dirname, 'src', 'barcelona.js'),
     style: path.join(__dirname, 'src/public/css', 'manhattan.css'),
-    build: path.join(__dirname, 'dist')
+    build: path.join(__dirname, '/build')
 };
 
-let common = {
+let config = {
+    devtool: 'source-map',
     entry: {
         app: PATHS.app
     },
     output: {
         path: PATHS.build,
-        filename: 'app.js'
+        filename: 'app.js',
+        publicPath: '/build/'
     },
     module: {
         loaders: [{
                 test: /src\/.+.js$/,
                 exclude: /node_modules/,
-                loader: 'babel',
+                loader: 'babel-loader',
                 query: {
                     "presets": [
                         "react", "es2015", "stage-0"
                     ]
                 }
             },
+            { test: /\.css$/, loader: "style!css" },
+            { test: /bootstrap.+\.(jsx|js)$/, loader: 'imports?jQuery=jquery,$=jquery,this=>window' },
             { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
             { test: /\.png$/, loader: "url-loader?limit=100000" },
             { test: /\.jpg$/, loader: "file-loader" },
@@ -39,36 +44,41 @@ let common = {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({ title: 'Barcelona' }),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
-        })
+        new CleanWebpackPlugin([PATHS.build], {
+            root: process.cwd()
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                // Drop `console` statements
+                drop_console: true
+            },
+            mangle: false,
+            beautify: true
+        }),
+        new webpack.optimize.DedupePlugin()
     ]
 };
 
-let config;
 
-switch (process.env.npm_lifecycle_event) {
-case 'build':
-    config = MergePlugin(parts.clean(PATHS.build),
-        common, { devtool: 'source-map' },
-        parts.setFreeVariable(
-            'process.env.NODE_ENV',
-            'production'
-        ),
-        parts.extractCSS(PATHS.style));
-    break;
-default:
-    config = MergePlugin(parts.clean(PATHS.build),
-        common, { devtool: 'source-map' },
-        parts.setFreeVariable(
-            'process.env.NODE_ENV',
-            'production'
-        ),
-        parts.extractCSS(PATHS.style));
-}
+// switch (process.env.npm_lifecycle_event) {
+// case 'build':
+//     config = MergePlugin(parts.clean(PATHS.build),
+//         common, { devtool: 'source-map' },
+//         parts.setFreeVariable(
+//             'process.env.NODE_ENV',
+//             'production'
+//         ),
+//         parts.extractCSS(PATHS.style));
+//     break;
+// default:
+//     config = MergePlugin(parts.clean(PATHS.build),
+//         common, { devtool: 'source-map' },
+//         parts.setFreeVariable(
+//             'process.env.NODE_ENV',
+//             'production'
+//         ),
+//         parts.extractCSS(PATHS.style));
+// }
 
 module.exports = validate(config);
