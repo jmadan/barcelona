8582c3291041d998d35d0e7f81d626ade3cc0430
Jasdeep Madan <jasdeepm@gmail.com>
2016-09-09 21:49:30 +0530

    created the social share component and imported in Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,14 @@
 import React from 'react';
+import SocialShare from './Utils/SocialShare';
 
 class Footer extends React.Component {
     render() {
         return (
             <div className="container">
 			<footer>
-				<div className="pull-left">
+				<div className="text-center">
 					<p className="text-center">Copyright-2016 - @Manhattan</p>
-				</div>
-				<div className="pull-right">
-				<span>
-					<i className="fa fa-twitter-square" aria-hidden="true"></i> 
-					<i className="fa fa-facebook-square" aria-hidden="true"></i>
-					<i className="fa fa-linkedin-square" aria-hidden="true"></i>
-				</span>
+					<SocialShare />
 				</div>
 			</footer>
 			</div>
